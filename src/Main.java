8cb287232ec76a101d37fbbdfcd86d272a457df7
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-10-22 01:06:47 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         int amount = 5000; // средняя стоимость билета
         System.out.println("Средняя стоимость билета - 5000 рублей");
-        int one_mile = 20; // за каждые 20 рублей начисляется 1 миля
+        int oneMile = 20; // за каждые 20 рублей начисляется 1 миля
         System.out.println("1 миля = 20 рублей");
-        int bonus = amount / one_mile;
+        int bonus = amount / oneMile;
 
         System.out.println("Количество бонусных миль: " + bonus + "  " + "бонусных миль");
 
